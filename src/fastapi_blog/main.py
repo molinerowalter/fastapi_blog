af9140d05736eab9cf699ce68f925a5b7d4da20e
molinerowalterd <molinerowalterd@gmail.com>
2026-09-22 20:15:10 -0300

    Validation Errors & Exceptions Handled. min 1:36:05

--- src/fastapi_blog/main.py
+++ src/fastapi_blog/main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request, HTTPException, status
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse 
 from fastapi.templating import Jinja2Templates
+from starlette.exceptions import HTTPException as StarletteHTTPException    
 from pathlib import Path
 
 # 1. Obtenemos la ruta absoluta del archivo actual (main.py)
@@ -42,5 +44,35 @@ def home(request: Request):
 def get_posts():
     return posts
 
+@app.get("/posts/{post_id}", include_in_schema=False) #Si retornamo HTML no lo mostraremos en la documentacion
+def get_post(request: Request, post_id: int):
+    for post in posts:
+        if post.get("id") == post_id:
+            return templates.TemplateResponse(request, "post.html", {"post": post, "title": post["title"]})
+    return templates.TemplateResponse(request, "error.html", {"status_code": status.HTTP_404_NOT_FOUND, "detail": "Post not found"})
+
 """ ARRIBA VEMOS LA DEFINICION DE LA API Y DOS ENDPOINTS """
 
+@app.exception_handler(StarletteHTTPException)
+def general_http_exception_handler(request: Request, exc: StarletteHTTPException):
+    message = (exc.detail if exc.detail else "An error occurred")
+
+    if request.url.path.startswith("/api"):
+        # Manejar excepciones HTTP para rutas que comienzan con /api
+        return JSONResponse(
+            status_code=exc.status_code,
+            content={"detail": message},
+        )
+    # Manejar excepciones HTTP generales
+    return templates.TemplateResponse(request, "error.html", {"status_code": exc.status_code, "title": exc.status_code, "detail": message}, status_code=exc.status_code)
+
+@app.exception_handler(RequestValidationError)
+def validation_exception_handler(request: Request, exc: RequestValidationError):
+    if request.url.path.startswith("/api"):
+        # Manejar excepciones de validación para rutas que comienzan con /api
+        return JSONResponse(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            content={"detail": exc.errors()},
+        )
+    # Manejar excepciones de validación generales
+    return templates.TemplateResponse(request, "error.html", {"status_code": status.HTTP_422_UNPROCESSABLE_ENTITY, "title": "Validation Error", "detail": "Invalid request. Please check your input and try again."}, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
