b5cff88da179cf620c8d665dace7e5db387644d6
molinerowalterd <molinerowalterd@gmail.com>
2026-09-22 16:47:13 -0300

    HTML Templates: Inheritance in templates with Jinja2

--- src/fastapi_blog/main.py
+++ src/fastapi_blog/main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from pathlib import Path
+
+# 1. Obtenemos la ruta absoluta del archivo actual (main.py)
+BASE_DIR = Path(__file__).resolve().parent
+
+# Si main.py está en src/fastapi_blog/main.py, subimos 3 niveles para llegar a fastapi_blog/
+TEMPLATES_DIR = BASE_DIR.parent.parent / "templates"
 
 app = FastAPI()
 
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory=str(TEMPLATES_DIR))
 posts: list[dict]=[
     {
         "id": 1,
@@ -22,12 +29,12 @@ posts: list[dict]=[
     }
 ]
 # include_in_schema=False hide the route in localhost:8000/docs, so it will not appear in the documentation
-@app.get("/", response_class=HTMLResponse, include_in_schema=False) #decorator para definir un endpoint, en este caso la ruta raiz "/" y especificamos que la respuesta sera en formato HTML
+@app.get("/", include_in_schema=False) #decorator para definir un endpoint, en este caso la ruta raiz "/" y especificamos que la respuesta sera en formato HTML
 @app.get("/posts") #decorator para definir un endpoint, en este caso la ruta "/posts" retornara la misma respuesta que home
 #Creamos un endpoint para esta url
-def home():
+def home(request: Request):
     #return {"message": "Welcome to the FastAPI Blog!"}  -- Respuesta en formato JSON
-    return f"<h1>{posts[0]['title']}</h1>" #--- Respuesta en formato HTML, retornando el titulo del primer post
+    return templates.TemplateResponse(request, "home.html", {"posts": posts, "title": "Home"}) #Retornamos la plantilla HTML home.html y pasamos el objeto request al contexto de la plantilla
 
 
 @app.get("/api/posts")
